dd2f25cb10b74dda31a8f100b7b3c937a5ce7652
molodoi <mr_stat12@hotmail.com>
2021-09-03 18:39:50 +0200

    Add EasyAdminBundle + Category|Product Entity

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Sf5 Ecommerce');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Categories', 'fas fa-list-alt', Category::class);
+        yield MenuItem::linkToCrud('Produits', 'fas fa-cube', Product::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
+    }
+}
---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            AssociationField::new('category'),
+            TextField::new('name'),
+            SlugField::new('slug')->setTargetFieldName('name'),
+            TextField::new('subtitle'),
+            TextareaField::new('description'),
+            MoneyField::new('price')->setCurrency('EUR'),
+            TextEditorField::new('content'),
+            ImageField::new('image')
+                ->setBasePath('products/')
+                ->setUploadDir('public/products/')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setRequired(false),
+        ];
+    }
+}
