4e4bc80c439e81d44585fbf03929c560db87fa45
molodoi <mr_stat12@hotmail.com>
2021-09-19 23:47:27 +0200

    Add secure admin backend

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
@@ -12,14 +15,17 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('lastname', 'Nom'),
+            TextField::new('firstname', 'Prénom'),
+            EmailField::new('email', 'Email'),
+            ChoiceField::new('roles', 'Roles')
+                    ->allowMultipleChoices()
+                    ->autocomplete()
+                    ->setChoices(['User' => 'ROLE_USER', 'Admin' => 'ROLE_ADMIN'])
         ];
     }
-    */
 }
